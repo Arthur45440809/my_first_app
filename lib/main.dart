d447050b72596f158fe9cec826a78d75ec6aa41c
Arthur45440809 <Arthur45440809@gmail.com>
2022-09-30 02:00:52 +0800

    screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,36 @@ import 'package:flutter/material.dart';
 void main() {
   const appTitle = Text('我的第一個Flutter App'),
       hiFlutter = Text(
-        'Hi, Flutter.',
-        style: TextStyle(fontSize: 50),
+        "Hi, Flutter.\n今天是220928",
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Colors.red,
+          decoration: TextDecoration.underline,
+        ),
       );
 
+  var img = Image.network(
+      "http://sprite.phys.ncku.edu.tw/astrolab/mirrors/apod/image/2109/SunSpotHill_Coy_2358.jpg");
+
   var appBody = Center(
-    child:hiFlutter,
+
+    child: hiFlutter,
+
+
   );
 
   var appBar = AppBar(
     title: appTitle,
+    backgroundColor: Colors.yellow[400],
   );
 
   var app = MaterialApp(
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+      backgroundColor: Color(0xFF00AEAE),
     ),
   );
   runApp(app);
-
 }
-
